1a03d89687a371fe5e62095e5f1cae5a6e16bd88
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-02-16 13:55:39 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<studio.h>
+#include <studio.h>
 /**
  * main - Entry point
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
