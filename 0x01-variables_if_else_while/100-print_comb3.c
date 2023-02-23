a571d5c016c5424f7482960f5377529b1a3a4504
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-02-23 10:43:16 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entyr point
+ * Description: prints two single
+ * digits combination
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				{
+					if (a != '8' || (a == '8' && b != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
