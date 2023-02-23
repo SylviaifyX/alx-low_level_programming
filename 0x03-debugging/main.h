c4cb82f058e556ed3651bc94dfb4b13bc471079a
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-02-22 22:09:53 -0800

    0 main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int convert_day(month, day);
+void print_remaining_days(int month, int day, int year)
+int largest_number(a, b, c);
+#endif /* MAIN_H */
