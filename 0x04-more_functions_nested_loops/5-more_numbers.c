8e0479fa4d1b1ce48a0639cacde106c9fc17fa7e
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-02-24 04:53:58 -0800

    print number 0-9

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - prints number from 0 to 9
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char a, c;
+	int i = 0;
+	
+	while (i < 10)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			c = a;
+			if (a > 9)
+			{
+				_putchar("1");
+				c = a % 10;
+			}
+			_putchar('0' + a);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
