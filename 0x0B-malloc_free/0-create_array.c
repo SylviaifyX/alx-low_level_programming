d4d2f7937b23c238cf52a2e82366bb8e1f0f0c7d
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-03-14 13:24:30 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0 || str == NULL)
 		return (NULL);
-	str = malloc(sizeof(char) * size);
+	str = (char *) malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
