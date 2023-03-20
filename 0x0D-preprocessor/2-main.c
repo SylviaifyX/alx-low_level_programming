d47daacb97207a08d931d2da38ce9b0c191abd3e
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-03-20 12:58:51 -0700

    preprocessor 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file when complied
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("The name of the file = %s\n", __FILE__);
+	return (0);
+}
