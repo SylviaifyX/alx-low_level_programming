7dbb5f738336b3e7797b1d9d778d4712cf7157af
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-03-31 07:48:37 -0700

    singly_linked list 101 helli holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-bits 64
-default rel
-
-section .data
-message db 'Hello, Holberton',0Ah ; Message to print
-
-section .text
-global main
-extern printf
-
+global   main
+	  extern    printf
 main:
-push message ; Push message address onto stack
-mov rdi, message ; set first argument to message address
-xor eax, eax ; Clear eax register
-call printf ; call printf function
-add rsp, 8 ; Remove message address from stack
-ret ; Return from main
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
