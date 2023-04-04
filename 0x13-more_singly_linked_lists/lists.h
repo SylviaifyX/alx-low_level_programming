7a804405b4ed26790a75e17dc25100128b476977
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-04-04 11:11:51 -0700

    singly link print listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef List_Header
-#define List_ Header
+#define List_Header
 
 
 
@@ -11,12 +11,22 @@
  * Description: singly linked list node structure
  *
  */
-
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+/**
+ * struct listp_s - linked single list
+ * @p: pointer to the node
+ * @next: points to the nexxt node
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -29,5 +39,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
+listint_t *reverse_listint(listint_t **head);
 #endif
