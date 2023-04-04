0457e33f313a587f9bf88c0404ec5672c55ebb53
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-04-04 11:45:08 -0700

    singly list 1istint

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * listint_len - function that returns the number of elements
+ * @h: point to code
+ *
+ * Return: element;
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counting = 1;
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		counting++;
+	}
+	return (counting);
+}
+
