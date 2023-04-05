f4ec2a16ff3405770861d8c973b5c4785e3afeb7
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-04-04 17:13:30 -0700

    signly link list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *
+ * free_listint2 - function that frees a listint_t list
+ * @head: point to first node
+ * NULL
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node_1, *new;
+
+	if (head != NULL)
+	{
+
+		node_1 = *head;
+
+		while ((new = node_1) != NULL)
+		{
+			node_1 = node_1->next;
+			free(new);
+		}
+
+		*head = NULL;
+	}
+}
