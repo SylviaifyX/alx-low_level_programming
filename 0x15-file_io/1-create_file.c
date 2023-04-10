9983d965aa8e73529b845cb9f4c416d522a24e9b
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-04-10 10:26:45 -0700

    file io create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, txt;
+	int i = 0, t,  txt;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,11 +22,12 @@ int create_file(const char *filename, char *text_content)
 			txt++;
 	}
 
-	txt = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	write(i, text_content, txt);
+	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	t = write(i, text_content, txt);
 	if (i == -1)
 		return (-1);
-
+	if (t == -1)
+		return (-1);
 	close(i);
 
 	return (1);
